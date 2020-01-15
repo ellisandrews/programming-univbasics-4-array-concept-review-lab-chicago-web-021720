4036ecd4b1f66b033aa677192351778763660df2
Ellis Andrews <ellisandrews1@gmail.com>
2020-01-15 23:28:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,10 @@
 def find_element_index(array, value_to_find)
-  curr_index = 0
   array.length.times do |index|
     curr_val = array[index]
     if curr_val == value_to_find
-      break
+      puts curr_val
   end
-  return nil
+  return puts "Not found"
 end
 
 def find_max_value(array)
