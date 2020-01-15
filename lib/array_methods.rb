d177791ea7ed9c6585a86183c6295123a0918e6f
Ellis Andrews <ellisandrews1@gmail.com>
2020-01-15 23:24:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,8 @@
 def find_element_index(array, value_to_find)
-  value_to_find_idx = 0
-  array.length.times { |index|
+  array.length.times do |index|
     if array[index] == value_to_find
-      puts index
-  }
+      return index
+  end
 end
 
 def find_max_value(array)
