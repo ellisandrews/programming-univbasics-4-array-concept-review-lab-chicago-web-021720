fe621a193737702924172d6a8625095c479b4a04
Ellis Andrews <ellisandrews1@gmail.com>
2020-01-15 23:31:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,5 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |index|
-    curr_val = array[index]
-    if curr_val == value_to_find
-      puts curr_val
-  end
-  return puts "Not found"
+  puts "ellis"
 end
 
 def find_max_value(array)
